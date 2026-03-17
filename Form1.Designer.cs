b3d7f5f36a99a3ea4005194873110b4e869a99a1
bryamsergeguerrero22-glitch <bryamsergeguerrero22@gmail.com>
2026-03-17 17:28:24 -0500

    17/03/2026 creacion de formulario de administrador aun le hace falta

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,9 +44,10 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.button1.Location = new System.Drawing.Point(155, 404);
+            this.button1.Location = new System.Drawing.Point(116, 328);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 31);
+            this.button1.Size = new System.Drawing.Size(62, 25);
             this.button1.TabIndex = 9;
             this.button1.Text = "Salir";
             this.button1.UseVisualStyleBackColor = false;
@@ -63,9 +64,11 @@
             this.groupBox1.Controls.Add(this.txt_correo_electronico);
             this.groupBox1.Controls.Add(this.pictureBox1);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(269, 12);
+            this.groupBox1.Location = new System.Drawing.Point(202, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(401, 461);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(301, 375);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             // 
@@ -73,9 +76,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.label3.Location = new System.Drawing.Point(70, 262);
+            this.label3.Location = new System.Drawing.Point(52, 213);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(84, 19);
+            this.label3.Size = new System.Drawing.Size(68, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Contraceña";
             // 
@@ -83,9 +87,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.label2.Location = new System.Drawing.Point(70, 201);
+            this.label2.Location = new System.Drawing.Point(52, 163);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 19);
+            this.label2.Size = new System.Drawing.Size(105, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Correo electronico";
             // 
@@ -94,18 +99,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkCyan;
-            this.label1.Location = new System.Drawing.Point(150, 23);
+            this.label1.Location = new System.Drawing.Point(112, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 29);
+            this.label1.Size = new System.Drawing.Size(88, 23);
             this.label1.TabIndex = 4;
             this.label1.Text = "SEMITEC";
             // 
             // btn_iniciar_sesion
             // 
             this.btn_iniciar_sesion.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.btn_iniciar_sesion.Location = new System.Drawing.Point(136, 333);
+            this.btn_iniciar_sesion.Location = new System.Drawing.Point(102, 271);
+            this.btn_iniciar_sesion.Margin = new System.Windows.Forms.Padding(2);
             this.btn_iniciar_sesion.Name = "btn_iniciar_sesion";
-            this.btn_iniciar_sesion.Size = new System.Drawing.Size(120, 35);
+            this.btn_iniciar_sesion.Size = new System.Drawing.Size(90, 28);
             this.btn_iniciar_sesion.TabIndex = 3;
             this.btn_iniciar_sesion.Text = "Iniciar Sesion";
             this.btn_iniciar_sesion.UseVisualStyleBackColor = false;
@@ -115,38 +122,43 @@
             // 
             this.txt_contraseña.BackColor = System.Drawing.SystemColors.Menu;
             this.txt_contraseña.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txt_contraseña.Location = new System.Drawing.Point(48, 284);
+            this.txt_contraseña.Location = new System.Drawing.Point(36, 231);
+            this.txt_contraseña.Margin = new System.Windows.Forms.Padding(2);
             this.txt_contraseña.Name = "txt_contraseña";
-            this.txt_contraseña.Size = new System.Drawing.Size(307, 27);
+            this.txt_contraseña.PasswordChar = '*';
+            this.txt_contraseña.Size = new System.Drawing.Size(231, 23);
             this.txt_contraseña.TabIndex = 2;
             // 
             // txt_correo_electronico
             // 
             this.txt_correo_electronico.BackColor = System.Drawing.SystemColors.Menu;
             this.txt_correo_electronico.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txt_correo_electronico.Location = new System.Drawing.Point(48, 223);
+            this.txt_correo_electronico.Location = new System.Drawing.Point(36, 181);
+            this.txt_correo_electronico.Margin = new System.Windows.Forms.Padding(2);
             this.txt_correo_electronico.Name = "txt_correo_electronico";
-            this.txt_correo_electronico.Size = new System.Drawing.Size(307, 27);
+            this.txt_correo_electronico.Size = new System.Drawing.Size(231, 23);
             this.txt_correo_electronico.TabIndex = 1;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = global::gestión_semillero_6trimestre.Properties.Resources.logo_proyecto_final_;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(136, 55);
+            this.pictureBox1.Location = new System.Drawing.Point(102, 45);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(120, 116);
+            this.pictureBox1.Size = new System.Drawing.Size(90, 94);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::gestión_semillero_6trimestre.Properties.Resources.fondo_loguin_semillero__2_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(951, 528);
+            this.ClientSize = new System.Drawing.Size(757, 479);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
---
+++ Form2.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace gestión_semillero_6trimestre
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnGestion_de_usuario = new System.Windows.Forms.Button();
+            this.btnGestion_de_Semilleros = new System.Windows.Forms.Button();
+            this.btnReportes = new System.Windows.Forms.Button();
+            this.btnTotalsemillero = new System.Windows.Forms.GroupBox();
+            this.Totalsemillerosactivos = new System.Windows.Forms.Label();
+            this.lblnumero = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Total_investigadores = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotal_investigadores = new System.Windows.Forms.Label();
+            this.Total_proyectos_en_curso = new System.Windows.Forms.GroupBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblproyectos_en_curso = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnMostrar_informacion = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.btnTotalsemillero.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Total_investigadores.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.Total_proyectos_en_curso.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btnReportes);
+            this.groupBox1.Controls.Add(this.btnGestion_de_Semilleros);
+            this.groupBox1.Controls.Add(this.btnGestion_de_usuario);
+            this.groupBox1.Controls.Add(this.btnDashboard);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(1, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 521);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menú";
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.BackColor = System.Drawing.Color.Transparent;
+            this.btnDashboard.Location = new System.Drawing.Point(0, 25);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(144, 41);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // btnGestion_de_usuario
+            // 
+            this.btnGestion_de_usuario.Location = new System.Drawing.Point(0, 72);
+            this.btnGestion_de_usuario.Name = "btnGestion_de_usuario";
+            this.btnGestion_de_usuario.Size = new System.Drawing.Size(144, 49);
+            this.btnGestion_de_usuario.TabIndex = 1;
+            this.btnGestion_de_usuario.Text = "Gestion de Usuario";
+            this.btnGestion_de_usuario.UseVisualStyleBackColor = true;
+            // 
+            // btnGestion_de_Semilleros
+            // 
+            this.btnGestion_de_Semilleros.Location = new System.Drawing.Point(0, 127);
+            this.btnGestion_de_Semilleros.Name = "btnGestion_de_Semilleros";
+            this.btnGestion_de_Semilleros.Size = new System.Drawing.Size(144, 54);
+            this.btnGestion_de_Semilleros.TabIndex = 2;
+            this.btnGestion_de_Semilleros.Text = "Gestion de Semilleros";
+            this.btnGestion_de_Semilleros.UseVisualStyleBackColor = true;
+            this.btnGestion_de_Semilleros.Click += new System.EventHandler(this.btnGestion_de_Semilleros_Click);
+            // 
+            // btnReportes
+            // 
+            this.btnReportes.Location = new System.Drawing.Point(0, 187);
+            this.btnReportes.Name = "btnReportes";
+            this.btnReportes.Size = new System.Drawing.Size(144, 44);
+            this.btnReportes.TabIndex = 3;
+            this.btnReportes.Text = "Reportes";
+            this.btnReportes.UseVisualStyleBackColor = true;
+            this.btnReportes.Click += new System.EventHandler(this.btnconfiguracion_Click);
+            // 
+            // btnTotalsemillero
+            // 
+            this.btnTotalsemillero.Controls.Add(this.pictureBox1);
+            this.btnTotalsemillero.Controls.Add(this.lblnumero);
+            this.btnTotalsemillero.Controls.Add(this.Totalsemillerosactivos);
+            this.btnTotalsemillero.Location = new System.Drawing.Point(151, 60);
+            this.btnTotalsemillero.Name = "btnTotalsemillero";
+            this.btnTotalsemillero.Size = new System.Drawing.Size(252, 59);
+            this.btnTotalsemillero.TabIndex = 2;
+            this.btnTotalsemillero.TabStop = false;
+            this.btnTotalsemillero.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // Totalsemillerosactivos
+            // 
+            this.Totalsemillerosactivos.AutoSize = true;
+            this.Totalsemillerosactivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Totalsemillerosactivos.Location = new System.Drawing.Point(64, 12);
+            this.Totalsemillerosactivos.Name = "Totalsemillerosactivos";
+            this.Totalsemillerosactivos.Size = new System.Drawing.Size(152, 16);
+            this.Totalsemillerosactivos.TabIndex = 0;
+            this.Totalsemillerosactivos.Text = "Total Semilleros Activos";
+            this.Totalsemillerosactivos.Click += new System.EventHandler(this.Totalsemillerosactivos_Click);
+            // 
+            // lblnumero
+            // 
+            this.lblnumero.AutoSize = true;
+            this.lblnumero.Location = new System.Drawing.Point(73, 41);
+            this.lblnumero.Name = "lblnumero";
+            this.lblnumero.Size = new System.Drawing.Size(107, 13);
+            this.lblnumero.TabIndex = 1;
+            this.lblnumero.Text = "contador de semillero";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Total_investigadores
+            // 
+            this.Total_investigadores.Controls.Add(this.pictureBox2);
+            this.Total_investigadores.Controls.Add(this.label1);
+            this.Total_investigadores.Controls.Add(this.lblTotal_investigadores);
+            this.Total_investigadores.Location = new System.Drawing.Point(409, 60);
+            this.Total_investigadores.Name = "Total_investigadores";
+            this.Total_investigadores.Size = new System.Drawing.Size(252, 59);
+            this.Total_investigadores.TabIndex = 3;
+            this.Total_investigadores.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(6, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(52, 49);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "contador investigadores";
+            // 
+            // lblTotal_investigadores
+            // 
+            this.lblTotal_investigadores.AutoSize = true;
+            this.lblTotal_investigadores.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal_investigadores.Location = new System.Drawing.Point(65, 8);
+            this.lblTotal_investigadores.Name = "lblTotal_investigadores";
+            this.lblTotal_investigadores.Size = new System.Drawing.Size(130, 16);
+            this.lblTotal_investigadores.TabIndex = 0;
+            this.lblTotal_investigadores.Text = "Total Investigadores";
+            // 
+            // Total_proyectos_en_curso
+            // 
+            this.Total_proyectos_en_curso.Controls.Add(this.pictureBox3);
+            this.Total_proyectos_en_curso.Controls.Add(this.label2);
+            this.Total_proyectos_en_curso.Controls.Add(this.lblproyectos_en_curso);
+            this.Total_proyectos_en_curso.Location = new System.Drawing.Point(667, 60);
+            this.Total_proyectos_en_curso.Name = "Total_proyectos_en_curso";
+            this.Total_proyectos_en_curso.Size = new System.Drawing.Size(252, 59);
+            this.Total_proyectos_en_curso.TabIndex = 4;
+            this.Total_proyectos_en_curso.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(6, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(52, 49);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "contador poyectos en curso";
+            // 
+            // lblproyectos_en_curso
+            // 
+            this.lblproyectos_en_curso.AutoSize = true;
+            this.lblproyectos_en_curso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblproyectos_en_curso.Location = new System.Drawing.Point(65, 8);
+            this.lblproyectos_en_curso.Name = "lblproyectos_en_curso";
+            this.lblproyectos_en_curso.Size = new System.Drawing.Size(124, 16);
+            this.lblproyectos_en_curso.TabIndex = 0;
+            this.lblproyectos_en_curso.Text = "Proyectos en Curso";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(151, 125);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(768, 400);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // btnMostrar_informacion
+            // 
+            this.btnMostrar_informacion.Location = new System.Drawing.Point(682, 531);
+            this.btnMostrar_informacion.Name = "btnMostrar_informacion";
+            this.btnMostrar_informacion.Size = new System.Drawing.Size(158, 37);
+            this.btnMostrar_informacion.TabIndex = 6;
+            this.btnMostrar_informacion.Text = "Mostrar Informacion";
+            this.btnMostrar_informacion.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::gestión_semillero_6trimestre.Properties.Resources.fondo_loguin_semillero__2_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(973, 609);
+            this.Controls.Add(this.btnMostrar_informacion);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Total_proyectos_en_curso);
+            this.Controls.Add(this.Total_investigadores);
+            this.Controls.Add(this.btnTotalsemillero);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.btnTotalsemillero.ResumeLayout(false);
+            this.btnTotalsemillero.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Total_investigadores.ResumeLayout(false);
+            this.Total_investigadores.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Total_proyectos_en_curso.ResumeLayout(false);
+            this.Total_proyectos_en_curso.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnGestion_de_usuario;
+        private System.Windows.Forms.Button btnGestion_de_Semilleros;
+        private System.Windows.Forms.Button btnReportes;
+        private System.Windows.Forms.GroupBox btnTotalsemillero;
+        private System.Windows.Forms.Label lblnumero;
+        private System.Windows.Forms.Label Totalsemillerosactivos;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox Total_investigadores;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal_investigadores;
+        private System.Windows.Forms.GroupBox Total_proyectos_en_curso;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblproyectos_en_curso;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnMostrar_informacion;
+    }
+}
